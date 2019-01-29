9dbabc78663b6cdb34a63ce0e4560c386d9092a8
arneish <arneish.p@gmail.com>
2019-01-29 19:27:11 +0530

    first commit

---
+++ cmake-build-debug/CMakeFiles/parallel_k_means.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallel_k_means.dir/main.cpp.o"
+  "parallel_k_means.pdb"
+  "parallel_k_means"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parallel_k_means.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
