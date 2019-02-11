211e2d394761136701f631330c13817deed33146
arneish <arneish.p@gmail.com>
2019-02-11 15:46:24 +0530

    final compilation scripts -O3 enabled

--- compile_pthread.sh
+++ compile_pthread.sh
@@ -1,5 +1,5 @@
 rm kmeans-pthread
-gcc main_pthread.c lab1_io.c a1_pthread.c -o kmeans-pthread -lpthread -fopenmp -lm
+gcc main_pthread.c lab1_io.c lab1_pthread.c -o kmeans-pthread -lpthread -fopenmp -lm -O3
 echo "Successful compilation of k-means-pthread.\nTo execute: 'sh run_pthread.sh <#clusters> <#threads> <input_file> <output_file> <centroids_file>'"
 echo "NOTE: Default compilation with pthread_mutex_lock() implementation. If pthread_spin_lock() is desired, include '-DUSE_SPINLOCK' flag during compilation"
 
