7f4c0e7fc2356a4fbb1a8429ffdfb2ea75932e22
arneish <arneish.p@gmail.com>
2019-02-11 15:46:24 +0530

    final compilation scripts -O3 enabled

--- compile_omp.sh
+++ compile_omp.sh
@@ -1,4 +1,4 @@
 rm kmeans-omp
-gcc lab1_io.c main_omp.c a2_openmp.c -o kmeans-omp -lm -fopenmp 
+gcc lab1_io.c main_omp.c lab1_omp.c -o kmeans-omp -lm -fopenmp -O3 
 echo "Successful compilation of k-means-OpenMP.\nTo execute: 'sh run_omp.sh <#clusters> <#threads> <input_file> <output_file> <centroids_file>'"
 
